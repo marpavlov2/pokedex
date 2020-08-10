5835a8a2f6a11934ca3d242c94639559db87f486
APKAVB35VMU7A2ICVLU5 <mariopavlovic1994@gmail.com>
2020-08-10 17:28:55 +0200

    Added all contacts page

---
+++ src/app/all-contacts/all-contacts.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-contacts',
+  templateUrl: './all-contacts.component.html',
+  styleUrls: ['./all-contacts.component.scss']
+})
+export class AllContactsComponent implements OnInit {
+  contacts: Array<any> = [
+    {name: 'Addie Hernande', liked: false},
+    {name: 'Oscar Arnold', liked: true},
+    {name: 'Isaiah McGuire', liked: false},
+    {name: 'Ann Schneider', liked: false},
+    {name: 'Agnes Terry', liked: false},
+    {name: 'Rose Bush', liked: false},
+    {name: 'Duane Reese', liked: true},
+    {name: 'Mae Changled', liked: false},
+    {name: 'Evelyn Weaver', liked: true},
+    {name: 'Catherine Moore', liked: true},
+    {name: 'Sam Manning', liked: false},
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllContactsComponent } from './all-contacts/all-contacts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AllContactsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
